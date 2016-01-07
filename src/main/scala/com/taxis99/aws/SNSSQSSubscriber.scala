279744baa03778ce775c30c2fe974782cb9e0135
Miguel Mendes Ruiz <migmruiz@gmail.com>
2016-01-07 19:19:26 -0200

    fices SNSSQSSubscriber class location

--- src/main/scala/com/taxis99/aws/SNSSQSSubscriber.scala
+++ src/main/scala/com/taxis99/aws/SNSSQSSubscriber.scala
@@ -7,29 +7,25 @@ import com.amazonaws.services.sns.{AmazonSNS, AmazonSNSAsyncClient}
 import com.amazonaws.services.sqs.model.CreateQueueRequest
 import com.amazonaws.services.sqs.{AmazonSQS, AmazonSQSAsyncClient}
 
-class SNSSQSSubscriber {
+class SNSSQSSubscriber (accessKey: String, secretKey: String, sqsEndpoint: String, snsEndpoint: String) {
 
-  class SNSSQSSubscriber (accessKey: String, secretKey: String, sqsEndpoint: String, snsEndpoint: String) {
+  def createSNSClient(): AmazonSNS = new AmazonSNSAsyncClient(new BasicAWSCredentials(accessKey, secretKey))
+  def createSQSClient(): AmazonSQS = new AmazonSQSAsyncClient(new BasicAWSCredentials(accessKey, secretKey))
 
-    def createSNSClient(): AmazonSNS = new AmazonSNSAsyncClient(new BasicAWSCredentials(accessKey, secretKey))
-    def createSQSClient(): AmazonSQS = new AmazonSQSAsyncClient(new BasicAWSCredentials(accessKey, secretKey))
+  private lazy val (snsClient, sqsClient) = {
 
-    private lazy val (snsClient, sqsClient) = {
+    val newSqsClient = createSQSClient()
+    newSqsClient.setEndpoint(sqsEndpoint)
 
-      val newSqsClient = createSQSClient()
-      newSqsClient.setEndpoint(sqsEndpoint)
-
-      val newSnsClient = createSNSClient()
-      newSnsClient.setEndpoint(snsEndpoint)
-      (newSnsClient, newSqsClient)
-    }
-
-    def subscribeQueueToTopic(queueName: String, topicName: String, rawMessageDelivery: Boolean = true) = {
-      val queueUrl = sqsClient.createQueue(new CreateQueueRequest(queueName)).getQueueUrl
-      val topicArn = snsClient.createTopic(new CreateTopicRequest(topicName)).getTopicArn
-      val subscriptionArn = Topics.subscribeQueue(snsClient, sqsClient, topicArn, queueUrl)
-      snsClient.setSubscriptionAttributes(subscriptionArn, "RawMessageDelivery", rawMessageDelivery.toString)
-    }
+    val newSnsClient = createSNSClient()
+    newSnsClient.setEndpoint(snsEndpoint)
+    (newSnsClient, newSqsClient)
   }
 
+  def subscribeQueueToTopic(queueName: String, topicName: String, rawMessageDelivery: Boolean = true) = {
+    val queueUrl = sqsClient.createQueue(new CreateQueueRequest(queueName)).getQueueUrl
+    val topicArn = snsClient.createTopic(new CreateTopicRequest(topicName)).getTopicArn
+    val subscriptionArn = Topics.subscribeQueue(snsClient, sqsClient, topicArn, queueUrl)
+    snsClient.setSubscriptionAttributes(subscriptionArn, "RawMessageDelivery", rawMessageDelivery.toString)
+  }
 }
