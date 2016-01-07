edff0146d8c5b6282e8d1b1230b4bb4933be41a6
Miguel Mendes Ruiz <migmruiz@gmail.com>
2016-01-07 18:47:01 -0200

    changing method and deprecated message

--- src/main/scala/com/taxis99/aws/S3Client.scala
+++ src/main/scala/com/taxis99/aws/S3Client.scala
@@ -7,13 +7,15 @@ import com.amazonaws.services.s3.AmazonS3Client
 
 class S3Client(accessKey: String, secretKey: String, bucketName: String) {
 
-  def createClient(): AmazonS3Client = new AmazonS3Client(new BasicAWSCredentials(accessKey, secretKey))
+  @deprecated("Use S3Client's create instead", since="v0.3.6" )
+  def createClient(): AmazonS3Client = create()
 
-  lazy val client = createClient()
+  def create(): AmazonS3Client = new AmazonS3Client(new BasicAWSCredentials(accessKey, secretKey))
+
+  lazy val client = create()
 
   def listFiles(prefix: String) = {
     client.listObjects(bucketName, prefix).getObjectSummaries.sortBy(_.getLastModified).reverse
   }
-
 
 }
--- src/main/scala/com/taxis99/aws/S3Helper.scala
+++ src/main/scala/com/taxis99/aws/S3Helper.scala
@@ -1,6 +1,6 @@
 package com.taxis99.aws
 
 
-@deprecated("Use S3Client instead", since="v0.3.5" )
+@deprecated("Use S3Client instead", since="v0.3.6" )
 class S3Helper(accessKey: String, secretKey: String, bucketName: String) extends S3Client(accessKey, secretKey, bucketName)
 
