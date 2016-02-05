1721a6d1f3e8ed1527906b150b14a30b9de7f113
Miguel Mendes Ruiz <migmruiz@gmail.com>
2016-02-05 13:02:35 -0200

    adds uploadFile method to s3 client

--- src/main/scala/com/taxis99/aws/S3Client.scala
+++ src/main/scala/com/taxis99/aws/S3Client.scala
@@ -1,9 +1,13 @@
 package com.taxis99.aws
 
+import java.io.{ File => JFile }
+
 import scala.collection.JavaConversions._
+import scala.reflect.io.File
 
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.{ AmazonS3, AmazonS3Client }
+import com.amazonaws.services.s3.model._
 
 /**
  * Client to handle S3 Interface
@@ -13,6 +17,10 @@ class S3Client(accessKey: String, secretKey: String, bucketName: String) {
   def create(): AmazonS3 = new AmazonS3Client(new BasicAWSCredentials(accessKey, secretKey))
 
   private lazy val client = create()
+
+  def uploadFile(key: String, file: JFile): PutObjectResult = client.putObject(bucketName, key, file)
+
+  def uploadFile(key: String, file: File): PutObjectResult = uploadFile(key, file.jfile)
 
   def listFiles(prefix: String) = {
     client.listObjects(bucketName, prefix).getObjectSummaries.sortBy(_.getLastModified).reverse
