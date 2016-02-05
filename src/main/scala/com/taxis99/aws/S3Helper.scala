ff6854ac3b94e778e16686d9af06615ac0f3ac63
Miguel Mendes Ruiz <migmruiz@gmail.com>
2016-02-05 03:35:05 -0200

    removes old deprecated helpers

--- src/main/scala/com/taxis99/aws/S3Helper.scala
+++
@@ -1,8 +0,0 @@
-package com.taxis99.aws
-
-/**
- * Helper to handle S3 Interface
- */
-@deprecated("Use S3Client instead", since="v0.3.6" )
-class S3Helper(accessKey: String, secretKey: String, bucketName: String) extends S3Client(accessKey, secretKey, bucketName)
-
--- src/main/scala/com/taxis99/aws/SQSHelper.scala
+++
@@ -1,7 +0,0 @@
-package com.taxis99.aws
-
-/**
- * Helper to handle SQS Interface
- */
-@deprecated("Use SQSClient instead", since="v0.3.6" )
-class SQSHelper(accessKey: String, secretKey: String, queueName: String, endpoint: String) extends SQSClient(accessKey, secretKey, queueName, endpoint)
